d378d049f8b0e3b32585a3daa726912d1f632a6b
slafniy <slafniy@gmail.com>
2025-01-19 18:58:30 +0400

    added batch helpers

---
+++ open_toolkit.bat
@@ -0,0 +1,4 @@
+set project=OneTimeRespecButton
+set glasses_exe_path="D:\SteamLibrary\steamapps\common\Baldurs Gate 3 Toolkit\Glasses.exe"
+
+%glasses_exe_path% -project %project% -lvl Basic_Level_A
